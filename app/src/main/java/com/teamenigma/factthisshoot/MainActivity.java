b1f9b10423cf15e61030f235ea0cf1878db373ee
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-10-03 17:44:18 +0800

    Added Settings Activity

--- app/src/main/java/com/teamenigma/factthisshoot/MainActivity.java
+++ app/src/main/java/com/teamenigma/factthisshoot/MainActivity.java
@@ -1,13 +1,38 @@
 package com.teamenigma.factthisshoot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button buttonStart;
+    Button buttonSettings;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonStart = (Button)findViewById(R.id.buttonStart);
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), ChooseCategory.class);
+                startActivity(i);
+            }
+        });
+
+        buttonSettings = (Button)findViewById(R.id.buttonSettings);
+        buttonSettings.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), Settings.class);
+                startActivity(i);
+            }
+        });
+
     }
 }
