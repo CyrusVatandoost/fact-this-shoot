4818cb6e63d14697459ab84d851eb8c6eee6f725
11502347 <rgee_gallega@dlsu.edu.ph>
2017-10-14 13:33:16 +0800

    Added DatabaseHelper Class

--- app/src/main/java/com/teamenigma/factthisshoot/MainActivity.java
+++ app/src/main/java/com/teamenigma/factthisshoot/MainActivity.java
@@ -6,15 +6,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import classes.Database.DatabaseHelper;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonStart;
     Button buttonSettings;
+    DatabaseHelper dataBase;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        dataBase = new DatabaseHelper(this);
 
         buttonStart = (Button)findViewById(R.id.buttonStart);
         buttonStart.setOnClickListener(new View.OnClickListener() {
