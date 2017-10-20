7a645955d21ff00c5cdf37e27673e01f35634e7a
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-10-20 22:21:28 +0800

    Change SpashScreen Time
    Displaying the splash screen now takes 1 second before going to the app.

--- app/src/main/java/com/teamenigma/factthisshoot/SplashActivity.java
+++ app/src/main/java/com/teamenigma/factthisshoot/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.teamenigma.factthisshoot;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,16 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        // Handler to wait 1 second.
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                gotoMainActivity();
+            }
+        }, 1000);
+    }
+
+    public void gotoMainActivity() {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
