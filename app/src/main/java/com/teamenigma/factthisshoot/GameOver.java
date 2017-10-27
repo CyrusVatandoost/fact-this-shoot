12e4f6917b8bb08ca102705626384dfacf5e394f
CyrusVatandoost <vatandoostcyrus@gmail.com>
2017-10-27 18:20:22 +0800

    Fixed Online Highscore Bug
    Switch statement does not work with strings. Learned the hard way.

--- app/src/main/java/com/teamenigma/factthisshoot/GameOver.java
+++ app/src/main/java/com/teamenigma/factthisshoot/GameOver.java
@@ -1,12 +1,10 @@
 package com.teamenigma.factthisshoot;
 
 import android.content.Intent;
-import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.games.Games;
@@ -48,6 +46,7 @@ public class GameOver extends AppCompatActivity {
         // Gets the score from the last activity (Game).
         Intent intent = getIntent();
         int score = intent.getIntExtra("score", 0);
+        String category = intent.getStringExtra("category");
         textViewScore.setText(score + "");
 
         findViewById(R.id.buttonLocalHighScores).setOnClickListener(new View.OnClickListener() {
@@ -66,19 +65,17 @@ public class GameOver extends AppCompatActivity {
             }
         });
 
-        switch(intent.getStringExtra("category")) {
-            case "dogs":
-                Games.Leaderboards.submitScore(singleton.getGoogleApiClient(), getBaseContext().getString(R.string.leaderboard_top_scorers_dogs), score);
-                Log.i("MyActivity", "Submitting high score for dogs");
-                break;
-            case "planets":
-                Games.Leaderboards.submitScore(singleton.getGoogleApiClient(), getBaseContext().getString(R.string.leaderboard_top_scorers_planets), score);
-                Log.i("MyActivity", "Submitting high score for planets");
-                break;
-            case "flowers":
-                Games.Leaderboards.submitScore(singleton.getGoogleApiClient(), getBaseContext().getString(R.string.leaderboard_top_scorers_flowers), score);
-                Log.i("MyActivity", "Submitting high score for flowers");
-                break;
+        if(category.equalsIgnoreCase("dogs")) {
+            Games.Leaderboards.submitScore(singleton.getGoogleApiClient(), getBaseContext().getString(R.string.leaderboard_top_scorers_dogs), score);
+            Log.i("GoogleApiClient", "Submitting high score for dogs: " + score);
+        }
+        else if(category.equalsIgnoreCase("planets")) {
+            Games.Leaderboards.submitScore(singleton.getGoogleApiClient(), getBaseContext().getString(R.string.leaderboard_top_scorers_planets), score);
+            Log.i("GoogleApiClient", "Submitting high score for planets: " + score);
+        }
+        else if(category.equalsIgnoreCase("flowers")) {
+            Games.Leaderboards.submitScore(singleton.getGoogleApiClient(), getBaseContext().getString(R.string.leaderboard_top_scorers_flowers), score);
+            Log.i("GoogleApiClient", "Submitting high score for flowers: " + score);
         }
 
     }
